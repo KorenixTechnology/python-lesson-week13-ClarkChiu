2e9e3e77d379250c31dfcb6e4f7b182145ff28d0
Clark Chiu <freebooters@gmail.com>
2020-04-24 21:48:28 +0800

    First Release

--- hw_week13.py
+++ hw_week13.py
@@ -1,6 +1,16 @@
 def hw_week13(number):
-    # Your code here
-    pass
+    try:
+        number = int(number)
+    except ValueError:
+        print('Please enter an positive integer.')
+        return False
+
+    while number != 1:
+        if number % 2 == 0:
+            number = number // 2
+        elif number % 2 == 1:
+            number = number * 3 + 1
+        print(number)
 
 
 if __name__ == '__main__':
